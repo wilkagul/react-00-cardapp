ace079fbb848f2a94b923b4ab3d76363586d1cff
wilkagul <wilkagul@gmail.com>
2023-02-10 00:11:11 +0300

    npx react boiler template

--- src/App.js
+++ src/App.js
@@ -1,29 +1,25 @@
-import logo from "./logo.svg";
-import "./App.css";
-import Card from "./components/Card";
-let arr = [
-  {
-    title: "Dağ 1",
-    par: "Açıklama 1",
-  },
-  {
-    title: "Dağ 2",
-    par: "Açıklama 2",
-  },
-  {
-    title: "Dağ 3",
-    par: "Açıklama 3",
-  },
-];
-const App = () => (
-  <div>
-    <h1>Başlık</h1>
-    <p>Başlangıç</p>
-    <div className="Cards">
-      {arr.map(({ par, title }) => (
-        <Card title={title} par={par} />
-      ))}
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
-  </div>
-);
+  );
+}
+
 export default App;
