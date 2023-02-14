5764a5742f3a9a584d5f9812c5ff10063a0ac897
wilkagul <wilkagul@gmail.com>
2023-02-14 16:27:58 +0300

    Topic cover

--- src/App.js
+++ src/App.js
@@ -1,29 +1,68 @@
-import logo from "./logo.svg";
+//import logo from "./logo.svg";
 import "./App.css";
 import Card from "./components/Card";
+import { useState } from "react";
 let arr = [
   {
+    id: 1,
     title: "Dağ 1",
     par: "Açıklama 1",
   },
   {
+    id: 2,
     title: "Dağ 2",
     par: "Açıklama 2",
   },
   {
+    id: 3,
     title: "Dağ 3",
     par: "Açıklama 3",
   },
 ];
-const App = () => (
-  <div>
-    <h1>Başlık</h1>
-    <p>Başlangıç</p>
-    <div className="Cards">
-      {arr.map(({ par, title }) => (
-        <Card title={title} par={par} />
-      ))}
+const App = () => {
+  const [lesson, setLesson] = useState(11);
+  return (
+    <div>
+      <h1>Başlık</h1>
+      <h2>Ders {lesson}</h2>
+
+      <button
+        onClick={() => {
+          setLesson(lesson + 1);
+        }}
+      >
+        Dersi Arttır
+      </button>
+
+      <button
+        onClick={() => {
+          setLesson(lesson - 1);
+        }}
+      >
+        Dersi Azalt
+      </button>
+
+      <button
+        onClick={() => {
+          setLesson(100);
+        }}
+      >
+        Ders 100
+      </button>
+
+      <p>Başlangıç</p>
+      <div className="Cards">
+        {arr.map(({ par, title }, i) => (
+          <Card
+            key={`index ${i}`}
+            title={title}
+            par={par}
+            lesson={lesson}
+            index={i}
+          />
+        ))}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 export default App;
